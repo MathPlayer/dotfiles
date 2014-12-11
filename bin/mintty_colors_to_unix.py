8d3f814273ad80159e6029641025c623c077673e
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2014-12-11 12:08:03 +0200

    bin: add color conversion script
    Add script to convert colors from minttyrc to native tty colors.
    
    Signed-off-by: Bogdan Popescu <bogdan.popescu0905@gmail.com>

---
+++ bin/mintty_colors_to_unix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+content = []
+with open(".minttyrc") as f:
+    content = f.readlines()
+
+if not content:
+    print("No content available")
+else:
+    for line in content:
+        ls = line.strip().split("=")
+        if len(ls) != 2:
+            continue
+        color=ls[0]
+        numbers=ls[1].split(",")
+        if len(numbers) != 3:
+            continue
+        newline = "echo -en \"\\e]P*"
+        for element in map(int, numbers):
+            newline += "{0:X}".format(element)
+        newline += "\" #{0}".format(color)
+        print newline
