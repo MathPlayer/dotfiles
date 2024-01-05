4bdb03c4ee1c27fca927c6d112feb2e11d42f7f7
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2024-01-05 04:05:17 +0100

    Use proper capitalization for neovim plugin

--- common/config/nvim/init.lua
+++ common/config/nvim/init.lua
@@ -359,7 +359,7 @@ require('lazy').setup({
 
   -- Autocompletion
   {
-    'L3mon4d3/LuaSnip',
+    'L3MON4D3/LuaSnip',
     version = 'v2.*',
     build = 'make install_jsregexp', -- optional
   },
