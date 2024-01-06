71648b0ace7e675e2d46a081b86003231dac312e
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2024-01-06 09:06:58 +0100

    Clean up Windows setup
    Too much time has passed since this was used, I only kelp relevant stuff
    for mintty and registry changes.

--- windows/bin/mintty_colors_to_unix.py
+++ windows/bin/mintty_colors_to_unix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 content = []
 with open(".minttyrc") as f:
@@ -11,12 +11,12 @@ else:
         ls = line.strip().split("=")
         if len(ls) != 2:
             continue
-        color=ls[0]
-        numbers=ls[1].split(",")
+        color = ls[0]
+        numbers = ls[1].split(",")
         if len(numbers) != 3:
             continue
         newline = "echo -en \"\\e]P*"
         for element in map(int, numbers):
             newline += "{0:X}".format(element)
         newline += "\" #{0}".format(color)
-        print newline
+        print(newline)
