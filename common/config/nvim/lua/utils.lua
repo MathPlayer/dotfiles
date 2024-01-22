c4426124b8a7bf9c7ec5677a319a9dfde8875cb7
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2024-01-22 15:12:03 +0100

    nvim: refactor personal lua utils

--- common/config/nvim/lua/utils.lua
+++ common/config/nvim/lua/utils.lua
@@ -1,12 +1,13 @@
-return {
-  HOME = os.getenv('HOME'),
-  map = function(mode, keys, func, desc, extra_opts)
-    extra_opts = extra_opts or {}
-    if desc then
-      extra_opts.desc = desc
-    end
-    vim.keymap.set(mode, keys, func, extra_opts)
-  end,
-}
+local M = {}
+
+M.map = function(mode, keys, func, desc, extra_opts)
+  extra_opts = extra_opts or {}
+  if desc then
+    extra_opts.desc = desc
+  end
+  vim.keymap.set(mode, keys, func, extra_opts)
+end
+
+return M
 
 -- vim: ts=2 sts=2 sw=2 et
