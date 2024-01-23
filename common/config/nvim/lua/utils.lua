4ca6c1469cec902320899eeb474eef723bbbecbf
Bogdan Popescu <bogdan.popescu0905@gmail.com>
2024-01-23 13:42:36 +0100

    nvim: fix mess with config between multiple machines
    Revert as much as possible the loss of config.

--- common/config/nvim/lua/utils.lua
+++ common/config/nvim/lua/utils.lua
@@ -1,13 +1,12 @@
-local M = {}
-
-M.map = function(mode, keys, func, desc, extra_opts)
-  extra_opts = extra_opts or {}
-  if desc then
-    extra_opts.desc = desc
-  end
-  vim.keymap.set(mode, keys, func, extra_opts)
-end
-
-return M
+return {
+  HOME = os.getenv('HOME'),
+  map = function(mode, keys, func, desc, extra_opts)
+    extra_opts = extra_opts or {}
+    if desc then
+      extra_opts.desc = desc
+    end
+    vim.keymap.set(mode, keys, func, extra_opts)
+  end,
+}
 
 -- vim: ts=2 sts=2 sw=2 et
